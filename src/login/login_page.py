aa1a0a0e160888a2ad9d062f2aeda4a80b6cbd1f
Aarón Espí <aaronespi@MacBook-Pro-de-Aaron.local>
2025-08-17 11:55:23 +0200

    pages getters removed

--- src/login/login_page.py
+++ src/login/login_page.py
@@ -6,6 +6,3 @@ class LoginPage:
     def __init__(self, driver):
         self.driver = driver
         self.login_form = LoginForm(driver)
-
-    def get_login_form(self):
-        return self.login_form
--- src/main/main_page.py
+++ src/main/main_page.py
@@ -1,11 +1,10 @@
+from common.top_bar import TopBar
 from common.search_widget import SearchWidget
 from common.product_grid import ProductGrid
-from common.top_bar import TopBar
 from config.environment_config import BASE_URL
 
 
 class MainPage:
-
     def __init__(self, driver):
         self.driver = driver
         self.top_bar = TopBar(driver)
@@ -14,12 +13,3 @@ class MainPage:
 
     def go_to(self):
         self.driver.get(BASE_URL)
-
-    def get_top_bar(self):
-        return self.top_bar
-
-    def get_search_widget(self):
-        return self.search_widget
-
-    def get_product_grid(self):
-        return self.product_grid
